556b30785abe6b1b268a199d1c9055ab05006ad1
sakib <mahbub.sakib@dsinnovators.com>
2024-05-20 23:29:26 +0600

    till v77-4 done

--- src/Dashboard/Payment.js
+++ src/Dashboard/Payment.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import Loading from '../Pages/Shared/Loading';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51PIXIYRsZhDCgnR6Q73kWgQ7pu4k7kynrZl7lp4LsKOgw8s48s60JpD9mzYB7ebFOYSmezxKBqWyuBxUD5Y7zdCe001f1a27F8');
 
 const Payment = () => {
     const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
+
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2 className='text-2xl text-purple-500'>Please pay for: {id}</h2>
+
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
+                    <p className='text-success font-bold'>Hello, {appointment.patientName}</p>
+                    <h2 className="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your appointment : <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                </div>
+            </div>
+
         </div>
     );
 };
